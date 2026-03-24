e554783519f033a39a9258ecbe748bd70471de16
chensheng <742611497@qq.com>
2026-03-24 22:19:08 +0800

    remove unused codes

--- convert-to-ico.ps1
+++
@@ -1,4 +0,0 @@
-[System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') | Out-Null
-$bmp = [System.Drawing.Bitmap]::FromFile('src-tauri\icons\icon.png')
-$bmp.Save('src-tauri\icons\icon.ico', [System.Drawing.Imaging.ImageFormat]::Icon)
-$bmp.Dispose()
